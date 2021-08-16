4f83afc182ca1184cc5075dca55c5c8a916eb34d
TurboKach <incognito@turbokach.me>
2021-08-16 14:57:45 +0300

    assert msg fix

--- tests/test_06_comment.py
+++ tests/test_06_comment.py
@@ -133,7 +133,7 @@ class Test06CommentAPI:
         pre_url = f'/api/v1/titles/{titles[0]["id"]}/reviews/{reviews[0]["id"]}/comments/'
         response = client.get(f'{pre_url}{comments[0]["id"]}/')
         assert response.status_code != 404, (
-            'Страница `/api/v1/titles/{title_id}/reviews/{review_id}/{review_id}/comments/{comment_id}/` '
+            'Страница `/api/v1/titles/{title_id}/reviews/{review_id}/comments/{comment_id}/` '
             'не найдена, проверьте этот адрес в *urls.py*'
         )
         assert response.status_code == 200, (
