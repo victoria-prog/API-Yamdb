dbf652b024fb6f1fbd84fc60b399f4839b75bcbc
yandex-praktikum <58176914+yandex-praktikum@users.noreply.github.com>
2021-11-08 17:06:19 +0300

    Update test_02_category.py

--- tests/test_02_category.py
+++ tests/test_02_category.py
@@ -102,12 +102,12 @@ class Test02CategoryAPI:
             'Проверьте, что при DELETE запросе `/api/v1/categories/{slug}/` удаляете категорию '
         )
         response = admin_client.get('/api/v1/categories/books/')
-        assert response.status_code == 405, (
-            'Проверьте, что при GET запросе `/api/v1/categories/{slug}/` возвращаете статус 405'
+        assert response.status_code == 404, (
+            'Проверьте, что при GET запросе `/api/v1/categories/{slug}/` возвращаете статус 404'
         )
         response = admin_client.patch('/api/v1/categories/books/')
-        assert response.status_code == 405, (
-            'Проверьте, что при PATCH запросе `/api/v1/categories/{slug}/` возвращаете статус 405'
+        assert response.status_code == 404, (
+            'Проверьте, что при PATCH запросе `/api/v1/categories/{slug}/` возвращаете статус 404'
         )
 
     def check_permissions(self, user, user_name, categories):
